0948314e8d6b3ad661774303b8119e78d86b9dba
Gabriel Sizilio <gabrielsizilio15@gmail.com>
2024-02-01 14:39:06 -0300

    feat: add search input

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Header from "../_components/header";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
@@ -10,10 +11,14 @@ export default function Home() {
 
       <div className="px-5 pt-5">
         <h2 className="text-xl font-bold ">Olá, Gabriel</h2>
-        <p className="capitalize">{format(new Date(), "EEEE',' dd 'de' MMMM", {
+        <p className="capitalize text-sm">{format(new Date(), "EEEE',' dd 'de' MMMM", {
           locale: ptBR
         })}
         </p>
+      </div>
+
+      <div className="px-5 pt-6">
+        <Search />
       </div>
     </div>
   );
